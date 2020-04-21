dae0424c739f79eb8ebb6612788216e13fa80385
rithiksubraveti <rithiksubraveti@gmail.com>
2020-04-21 22:03:04 +0000

    CA-2 initial file

---
+++ mobiledev-ca2/src/app/pages/episodes/episodes.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ApiService } from '../../services/api.service';
+import {NavController} from '@ionic/angular';
+@Component({
+  selector: 'app-episodes',
+  templateUrl: './episodes.page.html',
+  styleUrls: ['./episodes.page.scss'],
+})
+export class EpisodesPage implements OnInit {
+  episodes: Observable<any>;
+  constructor(private navController:NavController,private router: Router, private api: ApiService) { }
+  ngOnInit() {
+      this.episodes = this.api.getEpisodes();
+  }
+  openDetails(episode){
+      let episodeId= episode.episode_id;
+      this.router.navigateByUrl('/tabs/episodes/${episodeId}');
+  }
+}
